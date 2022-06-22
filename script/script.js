f6dc329962a4023894ca4e9184c7801a96370cbb
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-06-22 15:10:23 +0100

    first version of file reading attempted
    need to use XMLHTTPRequest

--- script/script.js
+++ script/script.js
@@ -1,14 +1,25 @@
-let body_object;
 
+let body_object;
 
 function newState(){
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     body_object.style.backgroundColor = '#' + randomColor;
+
+    const fr = new FileReader();
+    fr.onload() = function(){
+      document.getElementById('caption')
+                        .innerHTML=fr.result;
+    }
+
+    fr.readAsText("../assets/quotes.txt");
 }
+
+
 
 window.onload = function(){
     body_object = document.body;
     body_object.addEventListener("click", newState);
 
+    
    
 }
